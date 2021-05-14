92ddaf16f61e93bf9ac8d53e8e28441ba20eaf11
Bing <binchen561@gmail.com>
2021-05-14 16:00:35 +0800

    Fix ListShouldContainValueProxy to fit new trans logic

--- code/RegressionTest/List Should Contain Value.robot
+++ code/RegressionTest/List Should Contain Value.robot
@@ -7,9 +7,9 @@ Library    ../self_util.py
 
 *** Test Cases ***
 List should contain value
-    @{list1} =    Create List    Software    支援
+    @{list1} =    Create List    Software    Support
     # Log    ${dict1}
-    List Should Contain Value    ${list1}    Support
+    List Should Contain Value    ${list1}    Software
 
 List should not contain value
     @{list1} =    Create List    Support
--- code/listeners/proxyContainer/ListShouldContainValueProxy.py
+++ code/listeners/proxyContainer/ListShouldContainValueProxy.py
@@ -4,6 +4,8 @@ import sys
 from robot.libraries.Screenshot import Screenshot
 from robot.api import logger
 import I18nListener as i18n
+import ManyTranslations as ui
+from robot.utils import unic
 
 class ListShouldContainValueProxy(Proxy):
     def __init__(self, arg_format):
@@ -11,29 +13,51 @@ class ListShouldContainValueProxy(Proxy):
         # Fails if the ``value`` is not found from ``list``
     def i18n_Proxy(self, func):
         def proxy(self, list_, value, msg=None):            
-            if 'not' in func.__name__:
-                compare = lambda x,y:True if x != y else False
-            else:
-                compare = lambda x,y:True if x == y else False
-            # ListShouldContainValueProxy.show_warning(self, list_, value)
-            for list_translation in i18n.I18nListener.MAP.values(list_):
-                for value_translation in i18n.I18nListener.MAP.value(value):
-                    # logger.warn(type(list_translation))
-                    if compare(''.join(list_translation), value_translation):
-                        list_translation=[]
-                        for i in i18n.I18nListener.MAP.values(list_):
-                            list_translation += i 
-                        logger.warn(list_translation)
-                        logger.warn(value_translation)
-                        return func(self, list_translation, value_translation, msg)
-            return func(self, list_, value, msg)
+            #創出該次呼叫的參數紀錄
+            full_args = [str(list_), value] #將list轉str, 方便之後資料讀寫
+            
+            #翻譯
+            list_trans = i18n.I18nListener.MAP.values(list_, full_args)
+            value_trans = i18n.I18nListener.MAP.value(value, full_args)
+
+            list_have_multi_trans  = False
+            for lt in list_trans:
+                if len(lt) >1:
+                    list_have_multi_trans  = True
+                    break 
+
+            #遭遇一詞多譯
+            if list_have_multi_trans or len(value_trans)>1:
+                ListShouldContainValueProxy.show_warning(self, list_, value, full_args) #show warning
+                #檢查case會pass or fail
+                is_pass = False
+                if 'not' in func.__name__ :
+                    if value not in list_:
+                        is_pass = True
+                else:
+                    if value in list_:
+                        is_pass = True
+                if is_pass: #pass
+                    # 對預計開啟的UI做一些準備
+                    i18n.I18nListener.Is_Multi_Trans = True
+                    ui.UI.origin_xpaths_or_arguments.append(full_args)
+
+                    for i, lt in enumerate(list_trans):
+                        if len(lt)>1 and list_[i] not in ui.UI.translations_dict.keys(): #FIXME dict keys是否要在這邊判斷
+                            multi_trans_word = [list_[i]]                            # 還是要移交add_translations處理
+                            ui.UI.add_translations(self, multi_trans_word, lt)
+                    if len(value_trans) > 1 and value not in ui.UI.translations_dict.keys():
+                        multiple_translation_word = [value]     
+                        ui.UI.add_translations(self, multiple_translation_word, value_trans) #將翻譯詞加進等等UI會用到的dictionary中
+            #將處理好的翻譯回傳給robot原生keyword
+            return func(self, list_trans, value_trans, msg)
         return proxy
 
-    def show_warning(self, list_, value):
+    def show_warning(self, list_, value, full_args):
         language = 'i18n in %s:\n ' %i18n.I18nListener.LOCALE
         test_name = ('Test Name: %s') %BuiltIn().get_variable_value("${TEST NAME}") + '=> Exist multiple translations of the word' + '\n'
-        message_for_list = Proxy().deal_warning_message_for_list(list, 'List')
-        message_for_value = Proxy().deal_warning_message_for_one_word(value, 'Value')
-        if message_for_list != '' and message_for_value != '':
-            message = language + test_name + message_for_list + ' '*3 + '\n' + message_for_value + '\n' + 'You should verify translation is correct!'
+        message_for_list = Proxy().deal_warning_message_for_list(list_, full_args, 'LIST')
+        message_for_value = Proxy().deal_warning_message_for_one_word(value, full_args, 'VALUE')
+        if message_for_list or message_for_value:
+            message = language + test_name + message_for_list + '\n' + message_for_value + '\n' + 'You should verify translation is correct!'
             logger.warn(message)
