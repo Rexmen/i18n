c30e543a252e08bd53ab4da0e77711fbb05b6197
bing <binchen561@gmail.com>
2021-05-16 01:46:11 +0800

    Fix 'Remove Values From List' to fit new trans logic

--- code/RegressionTest/Remove Values From List.robot
+++ code/RegressionTest/Remove Values From List.robot
@@ -7,8 +7,7 @@ Library    ../self_util.py
 
 *** Test Cases ***
 Remove values from list
-    # @{list1} =    Create List    支援    軟體    蘋果
-    # Remove Values From List    ${list1}    支援
     @{list1} =    Create List    Support    Software
     Remove Values From List    ${list1}    Support
+    List Should Contain Value    ${list1}    軟體
     Log    ${list1}
--- code/listeners/proxyContainer/RemoveValuesFromListProxy.py
+++ code/listeners/proxyContainer/RemoveValuesFromListProxy.py
@@ -4,43 +4,67 @@ import sys
 from robot.libraries.Screenshot import Screenshot
 from robot.api import logger
 import I18nListener as i18n
+import ManyTranslations as ui
 
 class RemoveValuesFromListProxy(Proxy):
     def __init__(self, arg_format):
         arg_format[repr(['list_', 'values'])] = self
-        # 移除list中所有包含"values"的值 ，若values不存在list中，忽略
+        # 移除list中符合values的值 ，若values不存在list中，忽略
     def i18n_Proxy(self, func):
         def proxy(self, list_, *values):
-            # RemoveValuesFromListProxy.show_warning(self, list_, values)
-            # logger.warn(values)
-            values_translation = ()
-            for i in i18n.I18nListener.MAP.values(values):
-                temp_list=[]
-                for j in i:
-                    temp_list.append(j)
-                # logger.warn(temp_list)
-                values_translation += tuple(temp_list)
-            # logger.warn(values_translation)
+            #創出該呼叫的參數紀錄
+            full_args = [str(list_), str(values)]            
 
-            list_translation = []
-            for i in i18n.I18nListener.MAP.values(list_):
-                temp_list=[]
-                for j in i:
-                    temp_list.append(j)
-                list_translation += temp_list
-            list_.clear()
-            list_ += list_translation
-            logger.warn(list_)
+            #翻譯
+            list_trans = i18n.I18nListener.MAP.values(list_, full_args)
+            list_have_multi_trans  = False
+            for lt in list_trans:
+                if len(lt) >1:
+                    list_have_multi_trans  = True
 
-            for value in values_translation:
-                while value in list_:
-                    list_.remove(value)
+            values_trans = i18n.I18nListener.MAP.values(values, full_args)
+            values_have_multi_trans = False
+            for lt in values_trans:
+                if len(lt) >1:
+                    values_have_multi_trans = True
+                    break
+            
+            if list_have_multi_trans or values_have_multi_trans:
+                RemoveValuesFromListProxy.show_warning(self, list_, values, full_args) #show warning
+                #此case也沒有fail的問題，因為遇到key不在dictionary的話會自動忽略
+
+                # 對預計開啟的UI做一些準備
+                i18n.I18nListener.Is_Multi_Trans = True
+                ui.UI.origin_xpaths_or_arguments.append(full_args)
+                for i, lt in enumerate(list_trans):
+                    if len(lt)>1 and list_[i] not in ui.UI.translations_dict.keys(): #FIXME dict keys是否要在這邊判斷
+                        multi_trans_word = [list_[i]]                                # 還是要移交add_translations處理
+                        ui.UI.add_translations(self, multi_trans_word, lt)
+                for i, lt in enumerate(values_trans):
+                        if len(lt) > 1 and values[i] not in ui.UI.translations_dict.keys():
+                            multi_trans_word = [values[i]]     
+                            ui.UI.add_translations(self, multi_trans_word, lt) #將翻譯詞加進等等UI會用到的dictionary中
+            #將處理好的翻譯回傳給robot原生keyword
+            #參考listSelectionShouldBeProxy
+            # 將'list_的翻譯值' 賦予給 '原本的list_'，
+            # 否則之後並不會實際刪除list內的values
+            # 並且將list包list格式化成只有一層list
+            for i in range(len(list_)):
+                #格式化成string並過濾雜訊
+                list_[i] = str(list_trans[i]).strip("[]").replace(',','').replace('\'', '').replace(' ', '')
+            for i in range(len(values)):
+                #格式化成string並過濾雜訊    
+                values_trans[i] = str(values_trans[i]).strip("[]").replace(',','').replace('\'', '').replace(' ', '')
+            # logger.warn(list_)
+            return func(self, list_, *tuple(values_trans))
         return proxy
 
-    def show_warning(self, list_, *values):
+    def show_warning(self, list_, values, full_args):
         language = 'i18n in %s:\n ' %i18n.I18nListener.LOCALE
         test_name = ('Test Name: %s') %BuiltIn().get_variable_value("${TEST NAME}") + '=> Exist multiple translations of the word' + '\n'
-        message_for_values = Proxy().deal_warning_message_for_list(values, 'Values')
-        if message_for_values != '':
-            message = language + test_name + message_for_values + ' '*3 + '\n' + 'You should verify translation is correct!'
+        message_for_list = Proxy().deal_warning_message_for_list(list_, full_args, 'LIST')
+        message_for_values = Proxy().deal_warning_message_for_list(values, full_args, 'VALUES')
+        if message_for_list or message_for_values:
+            message = language + test_name + message_for_list + '\n' + \
+            message_for_values + '\n' + 'You should verify translation is correct!'
             logger.warn(message)
