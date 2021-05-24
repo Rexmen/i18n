3b39e92b71d92059568e20de61e01b5f146616fc
bing <binchen561@gmail.com>
2021-05-25 00:14:19 +0800

    modify ElementTextShouldBeProxy

--- code/listeners/proxyContainer/ElementTextShouldBeProxy.py
+++ code/listeners/proxyContainer/ElementTextShouldBeProxy.py
@@ -12,15 +12,20 @@ class ElementTextShouldBeProxy(Proxy):
     
     def i18n_Proxy(self, func):
         def proxy(self, locator, expected, message=None, ignore_case=False): #locator://*[@text='支援'](locator有時並不需要翻譯) expected:support
-            possible_translations = i18n.I18nListener.MAP.value(expected) #support可能map成支援/支援服務
+
+            full_args = [locator, expected]
+
+            possible_translations = i18n.I18nListener.MAP.value(expected, full_args) #support可能map成支援/支援服務
             actual_text = ''
             if len(possible_translations) > 1:
                 i18n.I18nListener.Is_Multi_Trans = True
+                ui.UI.origin_xpaths_or_arguments.append(full_args)
+
                 multiple_translation_words = []     
                 multiple_translation_words.append(expected) #將expected word包裝成list格式
                 # logger.warn(multiple_translation_words)
-                ui.add_translations(multiple_translation_words, possible_translations)
-                ElementTextShouldBeProxy.show_warning(self, expected, 'Expected')
+                ui.UI.add_translations(self, multiple_translation_words, possible_translations)
+                ElementTextShouldBeProxy.show_warning(self, expected, full_args)
                 BuiltIn().import_library('SeleniumLibrary')
                 actual_text = BuiltIn().run_keyword('Get Text', locator) #有多種翻譯，取得locator的字當作actual text(支援)
             else:
@@ -31,11 +36,11 @@ class ElementTextShouldBeProxy(Proxy):
                                 #locator://*[@text='支援'] , expected(support)以修正過後的actual text(支援)代替
         return proxy
     
-    def show_warning(self, expected, name):
+    def show_warning(self, expected, full_args):
         language = 'i18n in %s:\n ' %i18n.I18nListener.LOCALE
         test_name = ('Test Name: %s') %BuiltIn().get_variable_value("${TEST NAME}") + '=> Exist multiple translations of the word' + '\n'
-        message_for_expected = Proxy().deal_warning_message_for_one_word(expected, 'Expected')
-        if message_for_expected != '':
+        message_for_expected = Proxy().deal_warning_message_for_one_word(expected, full_args, 'Expected')
+        if message_for_expected :
             if expected not in i18n.I18nListener.Not_SHOW_WARNING_WORDS:   
                 message = language + test_name + message_for_expected + ' '*3 + '\n' + 'You should verify translation is correct!'
                 logger.warn(message)
